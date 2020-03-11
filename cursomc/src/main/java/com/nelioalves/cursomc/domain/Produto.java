c011d0674940c43d415cd22dda90bf074275483e
Leandro Oliveira <leandrolive@gmail.com>
2020-03-11 08:35:54 -0300

    ItemPedido e ItemPedidoPK

--- cursomc/src/main/java/com/nelioalves/cursomc/domain/Produto.java
+++ cursomc/src/main/java/com/nelioalves/cursomc/domain/Produto.java
@@ -1,9 +1,10 @@
 package com.nelioalves.cursomc.domain;
 
 import java.io.Serializable;
-
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -12,6 +13,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 
@@ -28,12 +30,14 @@ public class Produto implements Serializable {
 
 	@JsonBackReference
 	@ManyToMany
-	@JoinTable(name = "PRODUTO_CATEGORIA",
-		joinColumns = @JoinColumn(name = "produto_id"), 
-		inverseJoinColumns = @JoinColumn(name = "categoria_id"))
+	@JoinTable(name = "PRODUTO_CATEGORIA", joinColumns = @JoinColumn(name = "produto_id"), inverseJoinColumns = @JoinColumn(name = "categoria_id"))
 	private List<Categoria> categorias = new ArrayList<>();
 
-	public Produto() {}
+	@OneToMany(mappedBy = "id.produto")
+	private Set<ItemPedido> itens = new HashSet<>();
+
+	public Produto() {
+	}
 
 	public Produto(Integer id, String nome, Double preco) {
 		this.id = id;
@@ -73,6 +77,23 @@ public class Produto implements Serializable {
 		this.categorias = categorias;
 	}
 
+	public Set<ItemPedido> getItens() {
+		return itens;
+	}
+
+	public void setItens(Set<ItemPedido> itens) {
+		this.itens = itens;
+	}
+
+	public List<Pedido> getPedidos() {
+
+		List<Pedido> lista = new ArrayList<>();
+		for (ItemPedido x : itens) {
+			lista.add(x.getPedido());
+		}
+		return lista;
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -97,5 +118,4 @@ public class Produto implements Serializable {
 			return false;
 		return true;
 	}
-
 }
